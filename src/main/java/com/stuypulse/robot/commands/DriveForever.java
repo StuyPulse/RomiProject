5aea3b9d969d674759bf9a0071778f22882d9e64
BenG49 <64862590+BenG49@users.noreply.github.com>
2022-05-08 20:43:27 -0400

    Rename ExampleCommand to DriveForever

--- src/main/java/com/stuypulse/robot/commands/DriveForever.java
+++ src/main/java/com/stuypulse/robot/commands/DriveForever.java
@@ -1,20 +1,14 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package com.stuypulse.robot.commands;
 
 import com.stuypulse.robot.subsystems.Drivetrain;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/**
- * An Example command that drives forwards forever
- */
-public class ExampleCommand extends CommandBase {
+/** An Example command that drives forwards forever */
+public class DriveForever extends CommandBase {
     private final Drivetrain drivetrain;
 
-    public ExampleCommand(Drivetrain drivetrain) {
+    public DriveForever(Drivetrain drivetrain) {
         this.drivetrain = drivetrain;
 
         // This makes sure that if two commands both require the drivetrain,
